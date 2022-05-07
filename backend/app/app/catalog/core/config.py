16793f07a9bb09fc4cfcf1fcba4dea0f7177a444
MarianellaB <marianellabermudez@gmail.com>
2022-05-06 22:53:39 -0500

    .

---
+++ backend/app/app/catalog/core/config.py
@@ -0,0 +1,21 @@
+from ast import Str
+from pydantic import BaseSettings
+from typing import Optional
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    DATABASE_USERNAME: Str = 'postgres'
+    DATABASE_PASSWORD: str = '2111'
+    DATABASE_HOST: Str = 'localhost'
+    DATABASE_NAME: str = 'mydb'
+    DATABASE_URI: str = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5433/{DATABASE_NAME}"
+    
+    class config:
+        case_sensitive: bool=True
+        
+        
+@lru_cache
+def get_settings()-> Settings:
+    return Settings
+
+Settings= get_settings()
---
+++ backend/app/app/catalog/database/session.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from backend.app.core.config import Settings
+
+engine = create_engine(settings.DATABASE_URI)
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
